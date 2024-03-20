317682f8b99b59aa0b64c8bd98ee1349f8c0ee7d
mukundesh <mukundesh@outlook.com>
2024-03-20 08:43:03 +0530

    updated files

--- import/src/import_data.py
+++ import/src/import_data.py
@@ -71,8 +71,7 @@ def export_data(src_dir, tgt_dir):
     for src_file in todo_src_files:
         todo_code = get_code(src_file)
         #shutil.copyfile(src_file, tgt_dir / src_file.name)
-        print(f'copying {src_file} -> {tgt_dir / src_file.name}')
-
+        #print(f'copying {src_file} -> {tgt_dir / src_file.name}')
         if '.en.txt' in src_file.name:
             todo_info = get_tgt_info(src_dict[todo_code])
             tgt_dict[f'{todo_code}.pdf'] = todo_info
@@ -97,6 +96,7 @@ def main():
         src_dir = src_parent_dir / src_stub_dir
         
         export_data(src_dir, tgt_dir)
+    return 0
 
 main()
 
